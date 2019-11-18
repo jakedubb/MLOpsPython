5af325a6ccd8c19e60874e2e8cf9bade4c495285
jakedubb <jakedubb@users.noreply.github.com>
2019-11-18 15:19:08 -0800

    reverting changes

--- code/training/train.py
+++ code/training/train.py
@@ -30,14 +30,16 @@ from sklearn.datasets import load_diabetes
 from sklearn.linear_model import Ridge
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
-
 from sklearn.externals import joblib
 import numpy as np
 
+<<<<<<< HEAD
 from interpret.ext.blackbox import TabularExplainer
 from azureml.contrib.explain.model.explanation.explanation_client import (
  ExplanationClient
 )
+=======
+>>>>>>> parent of 68afd78... updated build pipeline with explainability
 
 parser = argparse.ArgumentParser("train")
 parser.add_argument(
@@ -63,7 +65,6 @@ release_id = args.release_id
 run = Run.get_context()
 exp = run.experiment
 ws = run.experiment.workspace
-client = ExplanationClient.from_run(run)
 
 X, y = load_diabetes(return_X_y=True)
 columns = ["age", "gender", "bmi", "bp", "s1", "s2", "s3", "s4", "s5", "s6"]
@@ -84,6 +85,7 @@ reg.fit(data["train"]["X"], data["train"]["y"])
 preds = reg.predict(data["test"]["X"])
 run.log("mse", mean_squared_error(preds, data["test"]["y"]))
 
+<<<<<<< HEAD
 # create an explainer to validate or debug the model
 tabular_explainer = TabularExplainer(reg,
                                      initialization_examples=X_train,
@@ -96,6 +98,12 @@ global_explanation = tabular_explainer.explain_global(X_test)
 # uploading model explanation data for storage or visualization
 comment = 'Global explanation on of Diabetes Regression'
 client.upload_model_explanation(global_explanation, comment=comment)
+=======
+
+# Save model as part of the run history
+
+# model_name = "."
+>>>>>>> parent of 68afd78... updated build pipeline with explainability
 
 with open(model_name, "wb") as file:
     joblib.dump(value=reg, filename=model_name)
